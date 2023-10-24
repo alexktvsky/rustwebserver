d3795d557de955abf97c3571f726c7e07566374c
Alex Kotovsky <alexktvsky@gmail.com>
2023-10-25 00:55:41 +0600

    Work in progress

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use rustwebserver::ThreadPool;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
@@ -10,7 +11,7 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "resources/www/hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "resources/www/index.html")
     }
     else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "resources/www/404.html")
@@ -25,9 +26,12 @@ fn handle_connection(mut stream: TcpStream) {
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
