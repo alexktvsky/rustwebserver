deb80daf796d59d869774f7b2bc377b55fd4565a
Alex Kotovsky <alexktvsky@gmail.com>
2023-10-25 20:01:13 +0600

    Work in progress

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,19 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+
 trait FnBox {
     fn call_box(self: Box<Self>);
 }
@@ -60,7 +73,7 @@ type Job = Box<dyn FnBox + Send + 'static>;
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>
+    thread: Option<thread::JoinHandle<()>>
 }
 
 impl Worker {
@@ -75,7 +88,7 @@ impl Worker {
 
         Worker {
             id,
-            thread
+            thread: Some(thread)
         }
     }
 }
